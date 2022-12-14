74579941ad840461e1d9d8d9004d295fee78ccec
codingdra <codingdra@gmail.com>
2022-12-15 05:47:43 +0700

    update title

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,9 @@ export const Header = (props) => {
             <div className='row'>
               <div className='col-md-8 col-md-offset-2 intro-text'>
                 <h1>
-                  {props.data ? props.data.title : 'Loading'}
+                  {props.data ? props.data.title1 : 'Loading'}
+                  {props.data ? props.data.title2 : 'Loading'}
+                  {props.data ? props.data.title3 : 'Loading'}
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : 'Loading'}</p>
